39d8f1cd58f0ba98dd1a29cb311abab90c776e5c
Sayf Tun-ismail <stun-ism@u90z02s03.42kl.edu.my>
2021-03-02 22:50:43 +0800

    completed ex00 ex01 ex02 and ex03

---
+++ ex01/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr);
+
+int main()
+{
+	int a;
+	int *ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+
+	a = 30;
+	printf("%d", a);
+	ptr = &a;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+
+	ft_ultimate_ft(ptr9);
+
+}
